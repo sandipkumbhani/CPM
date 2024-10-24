cbd442b307e19ada288755edd13b875b33fac13c
Kalpesh makwana <kalpesh.dev540@gmail.com>
2024-10-24 19:29:07 +0530

    added apiurls static class

---
+++ CPM.UI.Inftrastucture/Extension/ApiUrls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPM.UI.Inftrastucture.Extension
+{
+    public static class ApiUrls
+    {
+        public static string GetWeatherForecast { get; set; } = "WeatherForecast";
+
+    }
+}
