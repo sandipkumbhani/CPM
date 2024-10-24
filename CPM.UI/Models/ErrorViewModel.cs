0b12a3a4ea243421dbf73592bf48b14daaec2970
Kalpesh makwana <kalpesh.dev540@gmail.com>
2024-10-24 19:03:12 +0530

    Initial Merge

---
+++ CPM.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPM.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
